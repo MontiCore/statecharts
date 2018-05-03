c86e76c1689b40fdc8884cc2ad33d19822cd57a0
hoelldobler <hoelldobler@se-rwth.de>
2018-05-03 19:41:23 +0200

    - added test case for imports and do statement - renamed composing nonterminal

---
+++ scuse/src/main/java/util/Util.java
@@ -0,0 +1,24 @@
+package util;
+
+import de.monticore.prettyprint.IndentPrinter;
+import de.monticore.umlcd4a.cd4analysis._ast.ASTCD4AnalysisNode;
+import de.monticore.umlcd4a.prettyprint.CDPrettyPrinterConcreteVisitor;
+import de.monticore.umlsc.statechart._ast.ASTStatechartNode;
+import de.monticore.umlsc.statechart.prettyprint.StatechartPrettyPrinter;
+
+/**
+ * Created by
+ *
+ * @author KH
+ */
+public class Util {
+  public static String print(ASTStatechartNode node){
+    StatechartPrettyPrinter p  = new StatechartPrettyPrinter();
+    return p.prettyPrint(node);
+  }
+  
+  public static String print(ASTCD4AnalysisNode node){
+    CDPrettyPrinterConcreteVisitor p  = new CDPrettyPrinterConcreteVisitor(new IndentPrinter());
+    return p.prettyprint(node);
+  }
+}
