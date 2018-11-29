f95b10eafbd3964a821e23abb97e450dd7208236
Robert Eikermann <robert.eikermann@rwth-aachen.de>
2018-11-29 17:55:35 +0100

    Performance testing

--- scuse/src/test/java/tool/SCToolTest.java
+++ scuse/src/test/java/tool/SCToolTest.java
@@ -1,8 +1,19 @@
 package tool;
 
+import de.monticore.umlsc.statechart._ast.ASTSCArtifact;
+import de.monticore.umlsc.statechart.prettyprint.CountAST;
+import de.monticore.umlsc.statechartwithjava._parser.StatechartWithJavaParserTOP;
 import de.se_rwth.commons.logging.Log;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -24,5 +35,62 @@ public class SCToolTest {
     
     assertTrue(!false);
   }
-  
+
+  @Ignore
+  @Test
+  public void perf() throws IOException {
+
+    List<Optional<ASTSCArtifact>> scList = new LinkedList<>();
+
+
+    Double start = Double.valueOf(Long.valueOf(System.currentTimeMillis()).toString());
+
+    int rounds = 50;
+    for(int i = 0; i<rounds;i++) {
+      StatechartWithJavaParserTOP parser = new StatechartWithJavaParserTOP();
+      scList.add(parser.parse("src/test/models/Generated.sc"));
+    }
+    Double ende = Double.valueOf(Long.valueOf(System.currentTimeMillis()).toString());
+    System.out.println("Total Time");
+    System.out.println((ende-start)/1000D);
+
+    System.out.println("Time per Round");
+    System.out.println(((ende-start)/1000D)/Double.valueOf(new Double(rounds)));
+
+    CountAST counter = new CountAST();
+    for(Optional<ASTSCArtifact> a : scList) {
+      if(a.isPresent()) {
+
+        a.get().accept(counter);
+      }
+      else
+        System.out.println("a not present");
+    }
+
+    System.out.println(counter.count);
+
+  }
+
+  @Ignore
+  @Test
+  public void createSC() throws IOException {
+    File fOut = new File("src/test/models/Generated.sc");
+    FileWriter fw = new FileWriter(fOut);
+
+    fw.append("package models;\n");
+    fw.append("statechart Generated {\n");
+    int max = 100000;
+    for(int i = 0;i<max; i++) {
+      fw.append("state StateName"+ UUID.randomUUID().toString().replaceAll("-","") + " {\n");
+      fw.append("}\n\n");
+    }
+    fw.append("}");
+
+
+    fw.close();
+
+
+  }
+
+
 }
