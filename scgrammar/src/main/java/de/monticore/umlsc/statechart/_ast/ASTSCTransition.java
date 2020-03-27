b5a91b0aab4a04f394995c23b78cd414b240b17c
pichler <pichler@se-rwth.de>
2020-03-27 16:12:49 +0100

    add null check

--- scgrammar/src/main/java/de/monticore/umlsc/statechart/_ast/ASTSCTransition.java
+++ scgrammar/src/main/java/de/monticore/umlsc/statechart/_ast/ASTSCTransition.java
@@ -11,36 +11,41 @@ import de.monticore.umlsc.statechart._symboltable.SCStateSymbol;
 
 public class ASTSCTransition extends ASTSCTransitionTOP {
 
-	public ASTSCTransition() {
-		super();
-	}
+  public ASTSCTransition() {
+    super();
+  }
 
-	public ASTSCTransition(
-			Optional<de.monticore.umlsc.statechart._ast.ASTSCStereotype> stereotype,
-			String sourceName,
-			String targetName,
-			Optional<de.monticore.umlsc.statechart._ast.ASTSCTransitionBody> sCTransitionBody
-	) {
-		super();
-	}
+  public ASTSCTransition(
+      Optional<de.monticore.umlsc.statechart._ast.ASTSCStereotype> stereotype,
+      String sourceName,
+      String targetName,
+      Optional<de.monticore.umlsc.statechart._ast.ASTSCTransitionBody> sCTransitionBody
+  ) {
+    super();
+  }
 
-	public ASTSCState getTarget() {
-			IStatechartScope scope = getEnclosingScope();
-			String name = getTargetName();
-			Optional<SCStateSymbol> symbol = scope.resolveSCState(name);
-			if (symbol.isPresent()) {
-				return symbol.get().getAstNode();
-			}
-		return null;
-	}
-	public ASTSCState getSource() {
-			IStatechartScope scope = getEnclosingScope();
-			String name = getSourceName();
-			Optional<SCStateSymbol> symbol = scope.resolveSCState(name);
-			if (symbol.isPresent()) {
-				return symbol.get().getAstNode();
-			}
-		return null;
-	}
+  public ASTSCState getTarget() {
+    if (getEnclosingScope() != null) {
+      IStatechartScope scope = getEnclosingScope();
+      String name = getTargetName();
+      Optional<SCStateSymbol> symbol = scope.resolveSCState(name);
+      if (symbol.isPresent()) {
+        return symbol.get().getAstNode();
+      }
+    }
+    return null;
+  }
+
+  public ASTSCState getSource() {
+    if (getEnclosingScope() != null) {
+      IStatechartScope scope = getEnclosingScope();
+      String name = getSourceName();
+      Optional<SCStateSymbol> symbol = scope.resolveSCState(name);
+      if (symbol.isPresent()) {
+        return symbol.get().getAstNode();
+      }
+    }
+    return null;
+  }
 
 }
