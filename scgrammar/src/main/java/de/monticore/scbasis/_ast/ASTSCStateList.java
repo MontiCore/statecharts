f59c302f4f8b6f5378c0f2057a696f3a09a80380
Katrin Hölldobler <hoelldobler@se-rwth.de>
2020-07-16 17:07:14 +0200

    minor

--- scgrammar/src/main/java/de/monticore/scbasis/_ast/ASTSCStateList.java
+++ scgrammar/src/main/java/de/monticore/scbasis/_ast/ASTSCStateList.java
@@ -1,7 +1,7 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.scbasis._ast;
 
-public class ASTStateList extends ASTStateListTOP {
+public class ASTSCStateList extends ASTSCStateListTOP {
   @Override public String getName() {
     return getName(0);
   }
