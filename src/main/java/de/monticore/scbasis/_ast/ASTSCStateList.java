a97a379c1a3e1d159c85bfb6201030d932dccbac
Katrin Hölldobler <hoelldobler@se-rwth.de>
2020-08-06 11:45:58 +0200

    fixed generation

--- src/main/java/de/monticore/scbasis/_ast/ASTSCStateList.java
+++
@@ -1,8 +0,0 @@
-/* (c) https://github.com/MontiCore/monticore */
-package de.monticore.scbasis._ast;
-
-public class ASTSCStateList extends ASTSCStateListTOP {
-  @Override public String getName() {
-    return getNames(0);
-  }
-}
