51ddde89eb07335bfd0f68918435ae9680fd8873
Alex Lüpges <git.rwth@luepg.es>
2021-06-16 10:59:46 +0000

    Fix/reporting reachability and tests

--- src/main/java/de/monticore/scbasis/StateCollector.java
+++ src/main/java/de/monticore/scbasis/StateCollector.java
@@ -4,23 +4,48 @@ package de.monticore.scbasis;
 import de.monticore.scbasis._ast.ASTSCState;
 import de.monticore.scbasis._visitor.SCBasisVisitor2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 /**
- * Collects all states
+ * Collects all states.
+ * Also keeps track of their hierarchical level
  */
 public class StateCollector implements SCBasisVisitor2 {
 
 
-  protected final List<ASTSCState> states = new ArrayList<>();
+  protected final Map<ASTSCState, Integer> states = new HashMap<>();
 
   @Override
   public void visit(ASTSCState node) {
-    this.states.add(node);
+    this.states.put(node, this.getHierarchyLevel());
   }
 
-  public List<ASTSCState> getStates() {
-    return states;
+  public Collection<ASTSCState> getStates() {
+    return this.states.keySet();
+  }
+
+  /**
+   * Get all states of a given hierarchy level
+   * @param hierarchy the hierarchy level (0 being the root)
+   * @return a collection of states
+   */
+  public Collection<ASTSCState> getStates(int hierarchy) {
+    return this.states.entrySet().stream().filter(e -> e.getValue() == hierarchy).map(Map.Entry::getKey).collect(Collectors.toSet());
+  }
+
+  public Map<ASTSCState, Integer> getStatesMap() {
+    return this.states;
+  }
+
+  /**
+   * Returns the hierarchy level to keep track of in the {@link #states}
+   * By default 0, the root level is returned
+   * @return the hierarchy level
+   */
+  protected int getHierarchyLevel(){
+    return 0;
   }
 }
