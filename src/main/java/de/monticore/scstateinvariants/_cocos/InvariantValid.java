88c7c297ada73add45fb33b267d656be101b0b8b
David Pascal Schmalzing <schmalzing@se-rwth.de>
2023-01-18 10:01:58 +0100

    Check for sym type obscure

--- src/main/java/de/monticore/scstateinvariants/_cocos/InvariantValid.java
+++ src/main/java/de/monticore/scstateinvariants/_cocos/InvariantValid.java
@@ -2,21 +2,24 @@
 package de.monticore.scstateinvariants._cocos;
 
 import de.monticore.scstateinvariants._ast.ASTSCInvState;
-import de.monticore.types.check.TypeCalculator;
-import de.monticore.types.check.TypeCheck;
+import de.monticore.types.check.*;
 import de.se_rwth.commons.logging.Log;
 
 public class InvariantValid implements SCStateInvariantsASTSCInvStateCoCo {
+
   protected TypeCalculator deriveSymType;
-  
+
   public InvariantValid(TypeCalculator deriveSymType) {
     this.deriveSymType = deriveSymType;
   }
-  
+
   public static final String ERROR_CODE = "0xCC101";
-  
-  @Override public void check(ASTSCInvState node) {
-    if(!TypeCheck.isBoolean(deriveSymType.typeOf(node.getExpression()))){
+
+  @Override
+  public void check(ASTSCInvState node) {
+    SymTypeExpression symType = deriveSymType.typeOf(node.getExpression());
+
+    if (!symType.isObscureType() && !TypeCheck.isBoolean(symType)) {
       Log.error(ERROR_CODE + " Invariant of " + node.getName() + " not of type boolean", node.get_SourcePositionStart());
     }
   }
