c715b6a4ec825c584cad17949135ae97893213fc
n_jansen <nico.jansen1@rwth-aachen.de>
2023-03-03 15:27:24 +0100

    rename pkg.

--- src/testscgen/java/DoorExample/DoorExample.java
+++ src/testscgen/java/DoorExample/DoorExample.java
@@ -1,5 +1,5 @@
 /* (c) https://github.com/MontiCore/monticore */
-package doorexample;
+package DoorExample;
 
 public class DoorExample extends DoorExampleTOP {
     public void ringTheDoorBell() {
