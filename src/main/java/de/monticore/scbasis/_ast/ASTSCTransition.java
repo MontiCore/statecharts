9d670855daa867749f1b4eecc5e68c000528a212
Tom Bursch <tombursch@gmail.com>
2023-08-15 13:40:12 +0200

    Resolve SCState down in Transistions

--- src/main/java/de/monticore/scbasis/_ast/ASTSCTransition.java
+++ src/main/java/de/monticore/scbasis/_ast/ASTSCTransition.java
@@ -2,18 +2,18 @@
 package de.monticore.scbasis._ast;
 
 public class ASTSCTransition extends ASTSCTransitionTOP {
-  
+
   @Override
   protected void updateSourceNameSymbol() {
     if (getEnclosingScope() != null && (sourceNameSymbol == null || !getSourceName().equals(sourceNameSymbol.getName()))) {
-      sourceNameSymbol = getEnclosingScope().resolveSCStateMany(getSourceName()).stream().findFirst().orElse(null);
+      sourceNameSymbol = getEnclosingScope().resolveSCStateDownMany(getSourceName()).stream().findFirst().orElse(null);
     }
   }
 
   @Override
   protected void updateTargetNameSymbol() {
     if (getEnclosingScope() != null && (targetNameSymbol == null || !getTargetName().equals(targetNameSymbol.getName()))) {
-      targetNameSymbol = getEnclosingScope().resolveSCStateMany(getTargetName()).stream().findFirst().orElse(null);
+      targetNameSymbol = getEnclosingScope().resolveSCStateDownMany(getTargetName()).stream().findFirst().orElse(null);
     }
   }
 }
