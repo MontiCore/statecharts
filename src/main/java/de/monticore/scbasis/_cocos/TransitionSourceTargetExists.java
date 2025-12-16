760b1339dd99d39cfa7a2954590cdd905c771e9c
Vanol Nguemo Tadoum <nguemovanol@gmail.com>
2025-12-17 00:36:23 +0100

    minor

--- src/main/java/de/monticore/scbasis/_cocos/TransitionSourceTargetExists.java
+++ src/main/java/de/monticore/scbasis/_cocos/TransitionSourceTargetExists.java
@@ -22,15 +22,15 @@ public class TransitionSourceTargetExists implements SCBasisASTSCTransitionCoCo 
 
     if (!node.getSource().isPresentNameSymbol()) {
       Log.error(CANT_FIND_SOURCE_ERROR_CODE + " " + String.format(CANT_FIND_SOURCE_ERROR_MSG, node.getSource().getName()),
-        node.get_SourcePositionStart(),
-        node.get_SourcePositionEnd()
+        node.getSource().get_SourcePositionStart(),
+        node.getSource().get_SourcePositionEnd()
       );
     }
 
     if (!node.getTarget().isPresentNameSymbol()) {
       Log.error(CANT_FIND_TARGET_ERROR_CODE + " " + String.format(CANT_FIND_TARGET_ERROR_MSG, node.getTarget().getName()),
-        node.get_SourcePositionStart(),
-        node.get_SourcePositionEnd()
+        node.getTarget().get_SourcePositionStart(),
+        node.getTarget().get_SourcePositionEnd()
       );
     }
   }
